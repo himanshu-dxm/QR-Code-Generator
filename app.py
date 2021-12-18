8ea91a04300918649351591343b5fe9e118fffbd
himanshu <himanhu.behl@gmail.com>
2021-12-18 11:33:41 +0530

    Update: 1.1

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
+import qrcode
+import qrCodeGenerator
 
 app = Flask(__name__)
 
 
-@app.route('/generate',methods=["POST"])
+@app.route('/generate', methods=["POST"])
 def generate_qr():
     query = dict(request.form)['query']
-    img = generate(query)
+    # query = "https://qr-code-genrator.herokuapp.com/generate"
+    img = qrCodeGenerator.generate(query)
     res = "Image Created"
     json_file = {'query': res}
     return jsonify(json_file)
@@ -14,27 +17,6 @@ def generate_qr():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
-
-
-# A project to generate a QR code for an URL
-
-import qrcode
-# qr = qrcode.QRCode(
-#     version=1,
-#     error_correction=qrcode.constants.ERROR_CORRECT_L,
-#     box_size=10,
-#     border=4,
-# )
-# # Add your link below in the quoted place
-# qr.add_data('https://www.youtube.com/watch?v=Az9kvKlEbJU')
-#
-# qr.make(fit=True)
-#
-# # Making an image of the generated qr code
-# img = qr.make_image(fill_color="black", back_color="white")
-#
-# #Add the file name with which you want to save the QR code
-# img.save("file_name.png/jpg")
 
 
 def generate(url):
@@ -55,4 +37,3 @@ def generate(url):
     # Add the file name with which you want to save the QR code
     # img.save("file_name.png/jpg")
     return img
-
